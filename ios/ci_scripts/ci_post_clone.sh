7e7e6c08bea0d5eef5dfdd97b2514da3950606a1
Collin Gerken <collin.gerken@icloud.com>
2024-12-30 10:03:03 -0500

    Making the script executable

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -2,9 +2,9 @@
 
 # Fail this script if any subcommand fails.
 set -e
-PROJECT_ROOT=/
+
 # The default execution directory of this script is the ci_scripts directory.
-cd $PROJECT_ROOT # change working directory to the root of your cloned repo.
+cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
 # Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
